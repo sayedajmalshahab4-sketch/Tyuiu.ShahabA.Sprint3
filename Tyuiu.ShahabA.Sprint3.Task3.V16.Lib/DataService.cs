8069952f137d9eedbc15ce0946ad32710b4f7595
Ajmal <Ajmal@DESKTOP-01V489G>
2025-11-15 21:18:19 +0500

    Task3

---
+++ Tyuiu.ShahabA.Sprint3.Task3.V16.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.ShahabA.Sprint3.Task3.V16.Lib
+{
+    public class DataService : ISprint3Task3V16
+    {
+        public int GetCharCount(string value, char item)
+        {
+            int count = 0;
+
+            // Использование цикла foreach для подсчета букв
+            foreach (char ch in value)
+            {
+                if (ch == item)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
